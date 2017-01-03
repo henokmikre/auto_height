d39ce2261d5af3a6114582207eb543f74c24189c
Binu Varghese <myinbox.binu@gmail.com>
2017-01-03 15:16:57 +0530

    by Binu Varghese: libraries.yml and init-autoheight.js files updated.

--- js/init-autoheight.js
+++ js/init-autoheight.js
@@ -1,14 +1,28 @@
-(function ($, Drupal, drupalSettings) {
+/**
+ * @file
+ * Contains definition of the behaviour jsAutoHeight.
+ */
 
-  try
-  {
-    //$('.classname').AutoHeight();
-    $(drupalSettings.auto_height.selectors).AutoHeight();
-  }
-  catch (e) {
-    // catch errors, if any.
-    window.console && console.warn('jQuery Auto Height module stopped working with the exception:');
-    window.console && console.error(e);
-  }
+(function ($, Drupal, drupalSettings) {
+  "use strict";
+
+  Drupal.behaviors.jsAutoHeight = {
+    attach: function (context, settings) {
+
+      try {
+	  window.onload = Refresh;
+	  function Refresh() {
+        //$('.classname').AutoHeight();
+        $(drupalSettings.auto_height.selectors).AutoHeight();
+        }
+      }
+      catch (e) {
+        // catch errors, if any.
+        window.console && console.warn('jQuery Auto Height module stopped working with the exception:');
+        window.console && console.error(e);
+      }
+
+    }
+  };
 
 })(jQuery, Drupal, drupalSettings);
